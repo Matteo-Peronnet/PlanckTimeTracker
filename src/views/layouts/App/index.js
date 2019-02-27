dee65177e3a2cdb55d3f94c57b8a2adf2cf70bfb
Matteo Peronnet <matteo-peronnet@hotmail.fr>
2019-02-27 01:58:14 +0100

    Show hello world

---
+++ src/views/layouts/App/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>HELLO WORLD</h1>
+            </div>
+        );
+    }
+}
