cf6ed8671c645997a56a63fe5e1f5c2f21d68bdb
Matteo Peronnet <matteo-peronnet@hotmail.fr>
2019-02-27 01:38:45 +0100

    :sparkles: Adding Electron

---
+++ app/mainWindow.js
@@ -0,0 +1,24 @@
+const electron = require('electron');
+const { BrowserWindow } = electron;
+
+class MainWindow extends BrowserWindow {
+    constructor(url) {
+        super({
+            height: 500,
+            width: 300,
+            frame: false,
+            resizable: false,
+            show: false,
+            webPreferences: { backgroundThrottling: false }
+        });
+
+        this.loadURL(url);
+        this.on('blur', this.onBlur.bind(this));
+    }
+
+    onBlur() {
+        this.hide();
+    }
+}
+
+module.exports = MainWindow;
---
+++ index.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const electron = require('electron');
+const PlanckTray = require('./app/planckTray');
+const MainWindow = require('./app/mainWindow');
+
+const { app, /**ipcMain*/ } = electron;
+
+let mainWindow;
+let tray;
+
+app.on('ready', () => {
+
+    mainWindow = new MainWindow(`file://${__dirname}/src/index.html`);
+
+    const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'default-icon.png';
+    const iconPath = path.join(__dirname, `src/assets/${iconName}`);
+    tray = new PlanckTray(iconPath, mainWindow);
+});
